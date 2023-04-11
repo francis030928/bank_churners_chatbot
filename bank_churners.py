e0836eb4d72beb57d76452083ac5f5e94a32753f
francis030928 <francisoseghale03@gmail.com>
2023-04-11 17:52:15 +0100

    last_commit

--- bank_churners.py
+++ bank_churners.py
@@ -3,7 +3,7 @@ import streamlit as st
 # from sklearn.decomposition import PCA
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
